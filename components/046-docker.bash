e3c34af0f601275b218d09a389b9bb95e9a9d023
Isac Byeonghoon Yoo <bh322yoo@gmail.com>
2021-10-29 14:40:48 +0900

    Update 046-docker.bash

--- components/046-docker.bash
+++ components/046-docker.bash
@@ -9,7 +9,14 @@ sudo mkdir -p /etc/systemd/system/docker.service.d
 cat <<EOF | sudo tee /etc/systemd/system/docker.service.d/override.conf > /dev/null
 [Service]
 ExecStart=
-ExecStart=/usr/bin/dockerd -H fd:// -H tcp://127.0.0.1:2375
+ExecStart=/usr/bin/dockerd
+EOF
+
+sudo mkdir -p /etc/docker
+cat <<EOF | sudo tee /etc/docker/daemon.json > /dev/null
+{
+	hosts: [fd://, tcp://127.0.0.1:2375]
+}
 EOF
 
 sudo systemctl daemon-reload
