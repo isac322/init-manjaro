7170faf11bc0184133210e2a9f885d9301157252
Isac Byeonghoon Yoo <bh322yoo@gmail.com>
2021-10-30 21:41:06 +0900

    Update 066-mpv.bash

--- components/066-mpv.bash
+++ components/066-mpv.bash
@@ -3,5 +3,6 @@
 set -ex
 
 yay -S libplacebo rubberband uchardet luajit --noconfirm --removemake --asdeps --needed
-yay -S mpv-mpris libchardet --noconfirm --removemake --needed --assume-installed mpv
-yay -S mpv-git --noconfirm --removemake --noconfirm --removemake --needed
+yay -S mpv-git libchardet --noconfirm --removemake --needed
+yay -S mpv-mpris --noconfirm --removemake --needed
+yay -S mpv-git --noconfirm --removemake --rebuildall
