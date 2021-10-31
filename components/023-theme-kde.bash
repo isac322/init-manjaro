eca5aeac7da469e7616d7276145ce244d8795048
Isac Byeonghoon Yoo <bh322yoo@gmail.com>
2021-10-31 19:42:08 +0900

    Update 023-theme-kde.bash

--- components/023-theme-kde.bash
+++ components/023-theme-kde.bash
@@ -2,4 +2,5 @@
 
 set -ex
 
+yay -S extra-cmake-modules --noconfirm --needed --asdeps
 yay -S fluent-kde-theme-git fluent-gtk-theme-git fluent-decoration-git fluent-cursor-theme-git fluent-icon-theme-git ttf-fluentui-system-icons kvantum-qt5 --noconfirm --removemake --needed
