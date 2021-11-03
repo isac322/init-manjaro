4608c2cf49fa3aee4113ac65439986050d2bf3d6
Isac Byeonghoon Yoo <bh322yoo@gmail.com>
2021-11-03 11:27:35 +0900

    Update 002-makepkg.bash

--- components/002-makepkg.bash
+++ components/002-makepkg.bash
@@ -3,7 +3,10 @@
 set -ex
 
 # colorize pacman
-sudo sed -Ei 's/#Color/Color/' /etc/pacman.conf
+sudo sed -i -E 's/#Color/Color/' /etc/pacman.conf
 
 # optimize makepkg
-sudo sed -Ei "s/^\s*(PKG|SRC)EXT\s*=\s*'(.+)\.(gz|xz|zst)'\s*$/\1EXT='\2'/" /etc/makepkg.conf
+sudo sed -i -E "s/^\s*(PKG|SRC)EXT\s*=\s*'(.+)\.(gz|xz|zst)'\s*$/\1EXT='\2'/" /etc/makepkg.conf
+
+# SMP
+sudo sed -i -E 's/#?\s*MAKEFLAGS=("|'\'')(.*)-j[0-9]*(\W+.*)?("|'\'')/MAKEFlAGS=\1\2-j$(nproc)\3\4/' /etc/makepkg.conf
