eb11c043c812a87108d0261a7a830dec33c048e6
Isac Byeonghoon Yoo <bh322yoo@gmail.com>
2022-02-27 12:31:57 +0900

    Update 058-exfat.bash

--- components/058-exfat.bash
+++ components/058-exfat.bash
@@ -2,7 +2,7 @@
 
 set -ex
 
-if [ $(pacman -Qq exfat-utils) = 'exfat-utils' ]; then
+if [ "$(pacman -Qq exfat-utils)" = 'exfat-utils' ]; then
   sudo pacman -Rns exfat-utils --noconfirm
 fi
 yay -S exfatprogs --noconfirm --removemake --needed
