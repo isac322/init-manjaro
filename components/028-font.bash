a37e640af605bc7b4e229b4e8d9e13c45ec77a85
Byeonghoon Yoo <bhyoo@bhyoo.com>
2022-12-29 17:21:58 +0900

    refactor: update fonts

--- components/028-font.bash
+++ components/028-font.bash
@@ -2,4 +2,7 @@
 
 set -ex
 
-yay -S noto-fonts-emoji-apple ttf-nanum nerd-fonts-jetbrains-mono noto-fonts-cjk-kr-vf --noconfirm --removemake --needed
+yay -S \
+  ttf-apple-emoji \
+  ttf-nanum nerd-fonts-jetbrains-mono noto-fonts-cjk-kr-vf \
+  --noconfirm --removemake --needed
