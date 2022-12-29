c8a71725d452690e02aa17229dbc10da04d467c0
Byeonghoon Yoo <bhyoo@bhyoo.com>
2022-12-29 17:31:54 +0900

    refactor: enable ccache

--- components/002-makepkg.bash
+++ components/002-makepkg.bash
@@ -8,5 +8,10 @@ sudo sed -i -E 's/#Color/Color/' /etc/pacman.conf
 # optimize makepkg
 sudo sed -i -E "s/^\s*(PKG|SRC)EXT\s*=\s*'(.+)\.(gz|xz|zst)'\s*$/\1EXT='\2'/" /etc/makepkg.conf
 
+sudo pacman -Sy ccache --noconfirm --needed
+
+# optimize makepkg
+sudo sed -i -E 's/^([^#]+)!ccache/\1ccache/' /etc/makepkg.conf
+
 # SMP
 sudo sed -i -E 's/#?\s*MAKEFLAGS=("|'\'')(.*)-j[0-9]*(\W+.*)?("|'\'')/MAKEFlAGS=\1\2-j$(nproc)\3\4/' /etc/makepkg.conf
